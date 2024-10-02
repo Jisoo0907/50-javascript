4d1b5dee4ae02062e5c38c35bfcf9a3be249d35b
jisoolee <szjslee@gmail.com>
2024-10-03 03:16:47 +0900

    modify: input이 ul>li로 들어가도록 수정

--- src/js/todo.js
+++ src/js/todo.js
@@ -7,16 +7,18 @@ addButton.addEventListener("click", () => {
   // 버튼 클릭 시 추가하는 로직
   const checkBox = document.createElement("input");
   const newTodo = document.createElement("label");
+  const newTodoList = document.createElement("li");
   const removeButton = document.createElement("button");
 
   newTodo.className = "new-todo";
-  checkBox.type = "checkBox"; // input의 왼쪽에 오도록 할 수 없나?
+  checkBox.type = "checkBox";
   newTodo.innerText = todoInput.value;
   removeButton.className = "remove-button";
   removeButton.innerText = "삭제";
 
-  newTodo.appendChild(checkBox);
-  newTodo.appendChild(removeButton);
-  todoList.appendChild(newTodo);
+  newTodoList.appendChild(checkBox);
+  newTodoList.appendChild(newTodo);
+  newTodoList.appendChild(removeButton);
+  todoList.appendChild(newTodoList);
   todoInput.value = ""; // 리스트에 추가 후 input 비우기
 });
