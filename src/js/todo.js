bf55a0445ddc1b765eea88a54c2ff1aec1d73c3f
jisoolee <szjslee@gmail.com>
2024-10-09 00:40:38 +0900

    feat: 할 일 추가 - 수정 필요

--- src/js/todo.js
+++ src/js/todo.js
@@ -1,24 +1,41 @@
 const todoInput = document.querySelector("#todo-input"); // 입력한 할 일 가져오기
-const addButton = document.querySelector(".addButton");
-const todoList = document.querySelector(".list");
+const addButton = document.querySelector(".addButton"); // 추가 버튼
+const todoList = document.querySelector(".list"); // ul 요소
+const newTodo = document.createElement("label"); // input 요소(사용자가 입력한 할 일)
+const checkBox = document.createElement("input"); // checkbox
+const removeButton = document.createElement("button"); // 삭제 버튼
+const newTodoList = document.createElement("li"); // 입력한 할 일을 추가할 li 요소
+
+console.log(newTodo);
 
 /* 새 할 일 생성하기 */
 addButton.addEventListener("click", () => {
   // 버튼 클릭 시 추가하는 로직
-  const checkBox = document.createElement("input");
-  const newTodo = document.createElement("label");
-  const newTodoList = document.createElement("li");
-  const removeButton = document.createElement("button");
 
-  newTodo.className = "new-todo";
+  newTodo.className = "new-todo"; // label
   checkBox.type = "checkBox";
-  newTodo.innerText = todoInput.value;
+  newTodo.innerText = todoInput.value; // label
   removeButton.className = "remove-button";
   removeButton.innerText = "삭제";
 
   newTodoList.appendChild(checkBox);
-  newTodoList.appendChild(newTodo);
+  newTodoList.appendChild(newTodo); // label
   newTodoList.appendChild(removeButton);
   todoList.appendChild(newTodoList);
+  // 항목 여러개 추가가 안됨 이슈
   todoInput.value = ""; // 리스트에 추가 후 input 비우기
+});
+
+// 체크박스 체크 시 할 일 완료 됐다는 표시 되도록 (각 할 일 항목에 클릭 이벤트)
+todoList.addEventListener("click", () => {
+  if (checkBox.checked) {
+    newTodo.classList.add("checked"); // label
+  } else {
+    newTodo.classList.remove("checked"); // label
+  }
+});
+
+// 할 일 삭제하기
+removeButton.addEventListener("click", () => {
+  todoList.removeChild(newTodoList);
 });
